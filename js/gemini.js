8da915e74b33916a6fafc310861e1ae659a79595
Will Mercer <45526202+r0aringthunder@users.noreply.github.com>
2024-06-24 11:32:45 -0500

    Fully add Gemini to the portfolio

--- js/gemini.js
+++ js/gemini.js
@@ -1,11 +1,65 @@
-document.addEventListener("DOMContentLoaded", async () => {
-    const session = await window.ai.createTextSession();
-    const textarea = document.getElementById("promptTextArea");
-    const promptResponseText = document.getElementById("promptResponseText");
 
-    textarea.addEventListener("keydown", async () => {
-        let promptText = textarea.value.trim() === "" ? "Hi there" : textarea.value.trim();
-        const response = await session.prompt(promptText);
-        promptResponseText.textContent = response;
-    });
+function setError(error) {
+    document.getElementById("nano-error").dataset.error = error;
+}
+
+window.addEventListener("load", async function () {
+    try {
+        const hasAI = window.ai != null;
+
+        const hasNano =
+            (hasAI && (await window.ai.canCreateTextSession())) === "readily";
+
+        if (!hasNano) {
+            setError(!hasAI ? "not supported in this browser" : "not ready yet");
+            document.getElementById('how-to').dataset.help = true;
+            return;
+        }
+
+        const session = await window.ai.createTextSession();
+
+        const askElement = document.getElementById('ask-button');
+        const questionElement = document.getElementById('ask-question');
+        const responseElement = document.getElementById('ask-response');
+
+        let asking = false;
+
+        async function ask() {
+            if (asking) {
+                return;
+            }
+
+            const prompt = questionElement.value;
+            const stream = session.promptStreaming(prompt);
+            try {
+                asking = true;
+                askElement.disabled = true;
+                setError('');
+                for await (const chunk of stream) {
+                    responseElement.textContent = chunk;
+                }
+            } catch (err) {
+                setError(err.message);
+            }
+
+            asking = false;
+            askElement.disabled = false;
+        };
+
+        askElement.addEventListener('click', () => {
+            ask();
+        });
+
+        document.addEventListener('keydown', (event) => {
+            if (!asking && event.shiftKey && event.key === "Enter") {
+                askElement.focus();
+                event.preventDefault();
+                ask();
+            }
+        })
+
+        document.body.setAttribute("data-ready", "true");
+    } catch (err) {
+        setError(err.message);
+    }
 });
